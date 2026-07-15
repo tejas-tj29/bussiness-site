a1c57cb0013c8fb9212680e6929ec581161d960e
tejas-tj29 <btech10225.25@bitmesra.ac.in>
2026-07-16 00:13:57 +0530

    feat: update routing structure for admin dashboard and product management components

--- src/main.jsx
+++ src/main.jsx
@@ -9,15 +9,28 @@ import Contact from './pages/Contact.jsx'
 import Clients from './pages/Clients.jsx'
 import Products from './pages/Products.jsx'
 
+import AdminDashboard from './pages/AdminDashboard.jsx'
+import AdminAddProduct from './components/AdminAddProduct.jsx'
+import AdminManageProducts from './components/AdminManageProduct.jsx'
+import AdminLogin from './components/AdminLogin.jsx'
+
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <BrowserRouter>
       <Routes>
         <Route path="/" element={<Layout />}>
           <Route index element={<Home />} />
-          <Route path="/contact" element={<Contact />} />
-          <Route path="/clients" element={<Clients />} />
-          <Route path="/products" element={<Products />} />
+          <Route path="contact" element={<Contact />} />
+          <Route path="clients" element={<Clients />} />
+          <Route path="products" element={<Products />} />
+        </Route>
+        <Route path="/admin" element={<AdminDashboard />}>
+          <Route path="login" element={<AdminLogin />} />
+          <Route index element={<div className="text-gray-500 font-medium">Dashboard Overview Stats... (Coming Soon)</div>} />
+          <Route path="overview" element={<div className="text-gray-500 font-medium">Dashboard Overview Stats... (Coming Soon)</div>} />
+          <Route path="add-product" element={<AdminAddProduct />} />
+          <Route path="manage-products" element={<AdminManageProducts />} />
+          <Route path="inquiries" element={<div className="text-gray-500 font-medium">Client Inquiries Inbox... (Coming Soon)</div>} />
         </Route>
       </Routes>
     </BrowserRouter>
