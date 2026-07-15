c253f2b58c8a544eacfa8e42c71acd16a72922fb
tejas-tj29 <btech10225.25@bitmesra.ac.in>
2026-07-15 20:22:38 +0530

    feat: configure express middleware with cookie parser and enhanced request body limits

--- backend/src/app.js
+++ backend/src/app.js
@@ -1,5 +1,8 @@
 import express from 'express';
 import cors from 'cors';
+import cookieParser from 'cookie-parser';
+
+import authRouter from './routes/auth.routes.js';
 import inquiryRouter from './routes/inquiry.routes.js';
 import productRouter from './routes/product.routes.js';
 
@@ -12,12 +15,19 @@ app.use(cors({
 }
 ));
 
-app.use(express.json());
+app.use(express.json({limit:"20kb"}))
+
+app.use(express.urlencoded({extended:true,limit:"20kb"}))
+
+app.use(express.static("public"))
+
+app.use(cookieParser())
 
 app.get('/', (req, res) => {
     res.send('Welcome to the Sarawagi Enterprises API!');
 });
 
+app.use('/api/v1/auth', authRouter);
 app.use('/api/v1/products', productRouter);
 app.use('/api/v1/inquiries', inquiryRouter);
 
